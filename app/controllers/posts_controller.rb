ad91325d1371a3456dfc274c1ce5e2f5be25d144
manaraph <manaraph225@gmail.com>
2020-08-28 00:06:41 +0100

    Updates to post route

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,9 +3,21 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :string)
   end
 
 end
