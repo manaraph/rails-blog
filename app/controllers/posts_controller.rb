6dd8ab96d413af3dd7e8bf27a864c27d372b9f65
manaraph <manaraph225@gmail.com>
2020-08-28 01:42:49 +0100

    Update and edit posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,6 +19,20 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params;)
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
   private
   def post_params
     params.require(:post).permit(:title, :content)
